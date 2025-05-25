ce9133d1f344d5cdf027a05d406bc46552400cf8
thundercl3p <kiselyov_artyom@rambler.ru>
2025-05-25 20:42:44 +0300

    Update Transaction.h

--- banking/Transaction.h
+++ banking/Transaction.h
@@ -5,18 +5,15 @@ class Account;
 class Transaction {
  public:
   Transaction();
-  virtual ~Transaction();
+  ~Transaction();
 
   bool Make(Account& from, Account& to, int sum);
   int fee() const { return fee_; }
   void set_fee(int fee) { fee_ = fee; }
 
  private:
-  void Credit(Account& accout, int sum);
-  bool Debit(Account& accout, int sum);
-
-  // Virtual to test.
-  virtual void SaveToDataBase(Account& from, Account& to, int sum);
-
+  void Credit(Account& account, int sum);  
+    bool Debit(Account& account, int sum);   
+    void SaveToDataBase(Account& from, Account& to, int sum);
   int fee_;
 };
